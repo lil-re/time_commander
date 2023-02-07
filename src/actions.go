5b0ce10d5f6340797fbd8cdfbdd76c2b69a9109e
Lilian <dev@lil.re>
2023-02-07 13:29:54 +0100

    Code refactoring

--- src/actions.go
+++ src/actions.go
@@ -7,126 +7,124 @@ import (
 
 /*
 ** Actions
-*/
-func StartAction () {
+ */
+func StartAction() {
 	records := GetFileData()
 	now := time.Now()
 	date := now.Format("2006-01-02")
-  
-	if (len(records) == 0) {
-		newRecord := getNewRecord(date)
-		records = append(records, newRecord)
-		fmt.Println("Session has been started")
+
+	if len(records) == 0 {
+		createNewRecord(&records, date)
 	} else {
-	  record := &records[len(records) - 1]
-  
-	  if (date != record.Date) {
-		newRecord := getNewRecord(date)
-		records = append(records, newRecord)
-		fmt.Println("Session has been started")
-	  } else if len(record.Sessions) > 0 {
-		session := &record.Sessions[len(record.Sessions) - 1]
-  
-		if session.End == 0  {
-		  fmt.Println("Session has already been started")
-		} else {
-		  newSession := getNewSession()
-		  record.Sessions = append(record.Sessions, newSession)
-		  fmt.Println("Session has been started")
+		record := &records[len(records)-1]
+
+		if date != record.Date {
+			createNewRecord(&records, date)
+		} else if len(record.Sessions) > 0 {
+			session := &record.Sessions[len(record.Sessions)-1]
+
+			if session.End == 0 {
+				fmt.Println("Session has already been started")
+			} else {
+				createNewSession(record)
+			}
 		}
-	  }
 	}
 	SetFileData(records)
 }
-  
-func StopAction () {
+
+func StopAction() {
 	records := GetFileData()
-  
-	if (len(records) > 0) {
-	  record := &records[len(records) - 1]
-		
-	  if len(record.Sessions) > 0 {
-		session := &record.Sessions[len(record.Sessions) - 1]
-  
-		if session.End == 0  {
-		  session.End = GetCurrentTimestamp()
-		  SetFileData(records)
-		  fmt.Println("Session has been stopped")
-		} else {
-		  fmt.Println("Session is already stopped")
+
+	if len(records) > 0 {
+		record := &records[len(records)-1]
+
+		if len(record.Sessions) > 0 {
+			session := &record.Sessions[len(record.Sessions)-1]
+
+			if session.End == 0 {
+				session.End = GetCurrentTimestamp()
+				SetFileData(records)
+				fmt.Println("Session has been stopped")
+			} else {
+				fmt.Println("Session is already stopped")
+			}
 		}
-	  }
 	} else {
 		fmt.Println("There is no Record")
 	}
 }
-  
-func TodayAction () {
+
+func TodayAction() {
 	records := GetFileData()
-  
-	if (len(records) > 0) {
-	  record := records[len(records) - 1]
-  
-	  now := time.Now()
-	  date := now.Format("2006-01-02")
-  
-	  if (date == record.Date) {
-		floatDuration := 0.0
-  
-		for i := 0; i < len(record.Sessions); i++ {
-			floatDuration += getSessionDuration(record.Sessions[i])
+
+	if len(records) > 0 {
+		record := records[len(records)-1]
+
+		now := time.Now()
+		date := now.Format("2006-01-02")
+
+		if date == record.Date {
+			floatDuration := 0.0
+
+			for i := 0; i < len(record.Sessions); i++ {
+				floatDuration += getSessionDuration(record.Sessions[i])
+			}
+			printRecord("Today", floatDuration)
+		} else {
+			fmt.Println("There is no Record today")
 		}
-		printRecord("Today", floatDuration)
-	  } else {
-		fmt.Println("There is no Record today")
-	  }
 	}
 }
-  
-func ReportAction () {
+
+func ReportAction() {
 	records := GetFileData()
 	recordCounter := len(records)
 	recordLimit := recordCounter - Report - 1
-  
-	if (recordCounter > 0) {
-  
-	  for i := recordCounter - 1; i > recordLimit; i-- {
-		record := records[i]
-		floatDuration := 0.0
-  
-		for j := 0; j < len(record.Sessions); j++ {
-			floatDuration += getSessionDuration(record.Sessions[j])
+
+	if recordCounter > 0 {
+
+		for i := recordCounter - 1; i > recordLimit; i-- {
+			record := records[i]
+			floatDuration := 0.0
+
+			for j := 0; j < len(record.Sessions); j++ {
+				floatDuration += getSessionDuration(record.Sessions[j])
+			}
+			printRecord(record.Date, floatDuration)
 		}
-		printRecord(record.Date, floatDuration)
-	  }
 	} else {
-	  fmt.Println("There is no Record")
+		fmt.Println("There is no Record")
 	}
 }
 
-func getNewRecord (date string) Record {
+func createNewRecord(records *[]Record, date string) {
 	newSessions := make([]Session, 1)
 	newSessions[0].Start = GetCurrentTimestamp()
-	return Record{
-		Date: date,
+	newRecord := Record{
+		Date:     date,
 		Sessions: newSessions,
 	}
+	*records = append(*records, newRecord)
+	fmt.Println("Session has been started")
 }
 
-func getNewSession () Session {
-	return Session{
+func createNewSession(record *Record) {
+	newSession := Session{
 		Start: GetCurrentTimestamp(),
-		End: 0,
+		End:   0,
 	}
+	record.Sessions = append(record.Sessions, newSession)
+	fmt.Println("Session has been started")
 }
 
-func getSessionDuration (session Session) float64  {
+func getSessionDuration(session Session) float64 {
 	start := time.Unix(session.Start, 0)
 	end := time.Unix(session.End, 0)
 	return end.Sub(start).Seconds()
 }
 
-func printRecord (date string, floatDuration float64) {
+func printRecord(date string, floatDuration float64) {
 	textDuration := fmt.Sprintf("%vs", floatDuration)
 	parsedDuration, _ := time.ParseDuration(textDuration)
 	fmt.Printf("%v => %v\n", date, parsedDuration)
--- src/files.go
+++ src/files.go
@@ -1,37 +1,32 @@
 package src
 
-
 import (
-	"os"
-	"fmt"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"os"
 )
-
 
 /*
 ** Files
-*/
-func GetFileData () []Record {
+ */
+func GetFileData() []Record {
 	jsonFile, err := os.Open(Filename)
 	HandleError(err)
-  
+
 	defer jsonFile.Close()
-  
+
 	byteValue, err := ioutil.ReadAll(jsonFile)
 	HandleError(err)
-  
+
 	var data TimeCommanderData
 	json.Unmarshal(byteValue, &data)
-	fmt.Printf("%v \n", data)
 	return data.Records
 }
 
-  func SetFileData (records []Record) {
-	data := TimeCommanderData{ Records: records }
+func SetFileData(records []Record) {
+	data := TimeCommanderData{Records: records}
 	jsonData, err := json.MarshalIndent(data, "", "  ")
 	HandleError(err)
-	
+
 	_ = ioutil.WriteFile(Filename, jsonData, 0644)
 }
-  
